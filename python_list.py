9d6755dc13e8b540f061cd3d0ce4e6019939a931
Asmit Dhakal <107795383+Asmit-Dhakal@users.noreply.github.com>
2024-09-05 21:02:48 +0545

    push

--- python_list.py
+++ python_list.py
@@ -43,3 +43,50 @@ house = [["hallway", 11.25],
 print(house[4][1])
 # Subset the house list
 print(house[-1][1])
+
+# Create the areas list and make some changes
+areas = ["hallway", 11.25, "kitchen", 18.0, "chill zone", 20.0,
+         "bedroom", 10.75, "bathroom", 10.50]
+
+# Add poolhouse data to areas, new list is areas_1
+areas_1 = areas + ["poolhouse",24.5]
+
+# Add garage data to areas_1, new list is areas_2
+areas_2 = areas_1 + ["garage",15.45]
+
+areas = ["hallway", 11.25, "kitchen", 18.0,
+        "chill zone", 20.0, "bedroom", 10.75,
+         "bathroom", 10.50, "poolhouse", 24.5,
+         "garage", 15.45]
+
+# Delete the poolhouse items from the list
+del areas[10:12]
+
+# Print the updated list
+print(areas)
+
+
+# Create list areas
+areas = [11.25, 18.0, 20.0, 10.75, 9.50]
+
+# Change this command
+areas_copy = list(areas)
+
+# Change areas_copy
+areas_copy[0] = 5.0
+# Print areas
+print(areas)
+
+
+# Create variables var1 and var2
+var1 = [1, 2, 3, 4]
+var2 = True
+
+# Print out type of var1
+print(type(var1))
+
+# Print out length of var1
+print(len(var1))
+
+# Convert var2 to an integer: out2
+out2 = int(var2)
